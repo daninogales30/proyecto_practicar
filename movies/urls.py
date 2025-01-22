3a5f9e22c11507256ff46465fd9f5c721d7269ca
Daniel Nogales Mateos <dnogmat3011@fp.iesromerovargas.com>
2025-01-22 19:29:41 +0100

    Seguimos termiando, he hecho bastante

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,12 @@ from . import views
 app_name = 'movies'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:movie_id>/detail', views.movies_detail, name='movies_detail'),
+    path('actores/', views.lista_actores, name='lista_actores'),
+    path('actor/<int:actor_id>/detail', views.actor_detail, name='actor_detail'),
+    path('directors', views.lista_directores, name='lista_directores'),
+    path('directors/<int:director_id>/detail', views.director_detail, name='director_detail'),
+    path('2022/', views.pelis_2022, name='pelis_2022'),
+    path('rating/', views.pelis_rating, name='pelis_rating')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from movies.models import Movie
+from movies.models import *
 
 
 # Create your views here.
@@ -8,3 +8,31 @@ from movies.models import Movie
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', context={'movies': movies})
+
+def movies_detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/movies_detail.html', context={'movie': movie})
+
+def lista_actores(request):
+    actores = Actor.objects.all()
+    return render(request, 'movies/lista_actores.html', context={'actores': actores})
+
+def actor_detail(request, actor_id):
+    actor = get_object_or_404(Actor, pk=actor_id)
+    return render(request, 'movies/actor_detail.html', context={'actor': actor})
+
+def lista_directores(request):
+    directores = Director.objects.all()
+    return render(request, 'movies/lista_directores.html', context={'directores': directores})
+
+def director_detail(request, director_id):
+    director = get_object_or_404(Director, pk=director_id)
+    return render(request, 'movies/director_detail.html', context={'director': director})
+
+def pelis_2022(request):
+    peliculas = Movie.objects.filter(release_date__year='2022')
+    return render(request, 'movies/pelis_2022.html', context={'peliculas': peliculas})
+
+def pelis_rating(request):
+    peliculas = Movie.objects.filter(rating__gt=8)
+    return render(request, 'movies/pelis_rating.html', context={'peliculas': peliculas})
